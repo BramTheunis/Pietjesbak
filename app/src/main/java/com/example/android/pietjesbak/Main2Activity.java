3637f6a29237545815b7c4d4a843fba5493dc2d4
Bram Theunis <bramtheunis1997@gmail.com>
2019-01-22 13:38:28 +0100

    Scores per speler per worp added

--- app/src/main/java/com/example/android/pietjesbak/Main2Activity.java
+++ app/src/main/java/com/example/android/pietjesbak/Main2Activity.java
@@ -5,7 +5,6 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,10 +13,7 @@ import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
 import java.util.Random;
-import java.util.concurrent.TimeUnit;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -56,6 +52,17 @@ public class Main2Activity extends AppCompatActivity {
 
     boolean playerOne = true;
 
+    Integer value1;
+    Integer value2;
+    Integer value3;
+
+    Integer score1;
+    Integer score2;
+    Integer score3;
+
+    Integer totalScore1;
+    Integer totalScore2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -81,41 +88,39 @@ public class Main2Activity extends AppCompatActivity {
         dice1 = findViewById(R.id.dice1);
         dice2 = findViewById(R.id.dice2);
         dice3 = findViewById(R.id.dice3);
+        passTurn = findViewById(R.id.passTurn);
 
         c1 = findViewById(R.id.checkBox1);
         c2 = findViewById(R.id.checkBox2);
         c3 = findViewById(R.id.checkBox3);
 
-        // variabele turn
-
-
         rollDice.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (c1.isChecked() && c2.isChecked()) {
-                    int value3 = randomDiceValue();
+                    value3 = randomDiceValue();
 
                     int res3 = getResources().getIdentifier("dice_" + value3, "drawable", "com.example.android.pietjesbak");
 
                     dice3.setImageResource(res3);
 
                 } else if (c1.isChecked() && c3.isChecked()) {
-                    int value2 = randomDiceValue();
+                    value2 = randomDiceValue();
 
                     int res2 = getResources().getIdentifier("dice_" + value2, "drawable", "com.example.android.pietjesbak");
 
                     dice2.setImageResource(res2);
 
                 } else if (c2.isChecked() && c3.isChecked()) {
-                    int value1 = randomDiceValue();
+                    value1 = randomDiceValue();
 
                     int res1 = getResources().getIdentifier("dice_" + value1, "drawable", "com.example.android.pietjesbak");
 
                     dice1.setImageResource(res1);
 
                 } else if (c1.isChecked()) {
-                    int value2 = randomDiceValue();
-                    int value3 = randomDiceValue();
+                    value2 = randomDiceValue();
+                    value3 = randomDiceValue();
 
                     int res2 = getResources().getIdentifier("dice_" + value2, "drawable", "com.example.android.pietjesbak");
                     int res3 = getResources().getIdentifier("dice_" + value3, "drawable", "com.example.android.pietjesbak");
@@ -124,8 +129,8 @@ public class Main2Activity extends AppCompatActivity {
                     dice3.setImageResource(res3);
 
                 } else if (c2.isChecked()) {
-                    int value1 = randomDiceValue();
-                    int value3 = randomDiceValue();
+                    value1 = randomDiceValue();
+                    value3 = randomDiceValue();
 
                     int res1 = getResources().getIdentifier("dice_" + value1, "drawable", "com.example.android.pietjesbak");
                     int res3 = getResources().getIdentifier("dice_" + value3, "drawable", "com.example.android.pietjesbak");
@@ -134,8 +139,8 @@ public class Main2Activity extends AppCompatActivity {
                     dice3.setImageResource(res3);
 
                 } else if (c3.isChecked()) {
-                    int value1 = randomDiceValue();
-                    int value2 = randomDiceValue();
+                    value1 = randomDiceValue();
+                    value2 = randomDiceValue();
 
                     int res1 = getResources().getIdentifier("dice_" + value1, "drawable", "com.example.android.pietjesbak");
                     int res2 = getResources().getIdentifier("dice_" + value2, "drawable", "com.example.android.pietjesbak");
@@ -144,9 +149,9 @@ public class Main2Activity extends AppCompatActivity {
                     dice2.setImageResource(res2);
 
                 } else {
-                    int value1 = randomDiceValue();
-                    int value2 = randomDiceValue();
-                    int value3 = randomDiceValue();
+                    value1 = randomDiceValue();
+                    value2 = randomDiceValue();
+                    value3 = randomDiceValue();
 
                     int res1 = getResources().getIdentifier("dice_" + value1, "drawable", "com.example.android.pietjesbak");
                     int res2 = getResources().getIdentifier("dice_" + value2, "drawable", "com.example.android.pietjesbak");
@@ -156,6 +161,10 @@ public class Main2Activity extends AppCompatActivity {
                     dice2.setImageResource(res2);
                     dice3.setImageResource(res3);
                 }
+
+                howMuch1();
+                howMuch2();
+                howMuch3();
 
                 // beurt mechanisme
                 TextView turnPlayer1 = (TextView) findViewById(R.id.turnPlayer1);
@@ -170,6 +179,11 @@ public class Main2Activity extends AppCompatActivity {
                     tp1_number = tp1_number+1;
                     String tp1_new_text = tp1_number + "";
                     turnPlayer1.setText(tp1_new_text);
+
+                    String score1Text = score1 + score2 + score3 + "";
+                    TextView scorePlayer1 = (TextView) findViewById(R.id.scoreSpeler1);
+                    scorePlayer1.setText(score1Text);
+
                     if (tp1_number == 3) {
                         playerOne = false;
                         playerOneIndicator.setVisibility(View.INVISIBLE);
@@ -180,16 +194,32 @@ public class Main2Activity extends AppCompatActivity {
                     String tp2_new_text = tp2_number + "";
                     turnPlayer2.setText(tp2_new_text);
                     tp1_number = tp1_number-1;
+
+                    String score2Text = score1 + score2 + score3 + "";
+                    TextView scorePlayer2 = (TextView) findViewById(R.id.scoreSpeler2);
+                    scorePlayer2.setText(score2Text);
+
                     if (tp2_number-1 == tp1_number) {
                         // resetfunctie in de plaats zetten
                         playerOneIndicator.setVisibility(View.VISIBLE);
                         playerTwoIndicator.setVisibility(View.INVISIBLE);
                     }
                 }
-
-
             }
         });
+
+        passTurn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (playerOne == true) {
+                    playerOne = false;
+                    playerOneIndicator.setVisibility(View.INVISIBLE);
+                    playerTwoIndicator.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+
     }
 
     private void initializeWidgets() {
@@ -200,6 +230,8 @@ public class Main2Activity extends AppCompatActivity {
         scoreOne = (TextView) findViewById(R.id.scoreSpeler1);
         scoreTwo = (TextView) findViewById(R.id.scoreSpeler2);
     }
+
+
 
     private final SensorEventListener sensorListener = new SensorEventListener() {
         @Override
@@ -214,31 +246,33 @@ public class Main2Activity extends AppCompatActivity {
             float delta = acelVal - acelLast;
             shake = shake * 0.9f + delta;
 
-            if (shake > 12) {
+
+
+            if (shake > 16) {
                 if (c1.isChecked() && c2.isChecked()) {
-                    int value3 = randomDiceValue();
+                    value3 = randomDiceValue();
 
                     int res3 = getResources().getIdentifier("dice_" + value3, "drawable", "com.example.android.pietjesbak");
 
                     dice3.setImageResource(res3);
 
                 } else if (c1.isChecked() && c3.isChecked()) {
-                    int value2 = randomDiceValue();
+                    value2 = randomDiceValue();
 
                     int res2 = getResources().getIdentifier("dice_" + value2, "drawable", "com.example.android.pietjesbak");
 
                     dice2.setImageResource(res2);
 
                 } else if (c2.isChecked() && c3.isChecked()) {
-                    int value1 = randomDiceValue();
+                    value1 = randomDiceValue();
 
                     int res1 = getResources().getIdentifier("dice_" + value1, "drawable", "com.example.android.pietjesbak");
 
                     dice1.setImageResource(res1);
 
                 } else if (c1.isChecked()) {
-                    int value2 = randomDiceValue();
-                    int value3 = randomDiceValue();
+                    value2 = randomDiceValue();
+                    value3 = randomDiceValue();
 
                     int res2 = getResources().getIdentifier("dice_" + value2, "drawable", "com.example.android.pietjesbak");
                     int res3 = getResources().getIdentifier("dice_" + value3, "drawable", "com.example.android.pietjesbak");
@@ -247,8 +281,8 @@ public class Main2Activity extends AppCompatActivity {
                     dice3.setImageResource(res3);
 
                 } else if (c2.isChecked()) {
-                    int value1 = randomDiceValue();
-                    int value3 = randomDiceValue();
+                    value1 = randomDiceValue();
+                    value3 = randomDiceValue();
 
                     int res1 = getResources().getIdentifier("dice_" + value1, "drawable", "com.example.android.pietjesbak");
                     int res3 = getResources().getIdentifier("dice_" + value3, "drawable", "com.example.android.pietjesbak");
@@ -257,8 +291,8 @@ public class Main2Activity extends AppCompatActivity {
                     dice3.setImageResource(res3);
 
                 } else if (c3.isChecked()) {
-                    int value1 = randomDiceValue();
-                    int value2 = randomDiceValue();
+                    value1 = randomDiceValue();
+                    value2 = randomDiceValue();
 
                     int res1 = getResources().getIdentifier("dice_" + value1, "drawable", "com.example.android.pietjesbak");
                     int res2 = getResources().getIdentifier("dice_" + value2, "drawable", "com.example.android.pietjesbak");
@@ -267,9 +301,9 @@ public class Main2Activity extends AppCompatActivity {
                     dice2.setImageResource(res2);
 
                 } else {
-                    int value1 = randomDiceValue();
-                    int value2 = randomDiceValue();
-                    int value3 = randomDiceValue();
+                    value1 = randomDiceValue();
+                    value2 = randomDiceValue();
+                    value3 = randomDiceValue();
 
                     int res1 = getResources().getIdentifier("dice_" + value1, "drawable", "com.example.android.pietjesbak");
                     int res2 = getResources().getIdentifier("dice_" + value2, "drawable", "com.example.android.pietjesbak");
@@ -279,6 +313,10 @@ public class Main2Activity extends AppCompatActivity {
                     dice2.setImageResource(res2);
                     dice3.setImageResource(res3);
                 }
+
+                howMuch1();
+                howMuch2();
+                howMuch3();
 
                 TextView turnPlayer1 = (TextView) findViewById(R.id.turnPlayer1);
                 String tp1 = turnPlayer1.getText().toString();
@@ -292,6 +330,11 @@ public class Main2Activity extends AppCompatActivity {
                     tp1_number = tp1_number+1;
                     String tp1_new_text = tp1_number + "";
                     turnPlayer1.setText(tp1_new_text);
+
+                    String score1Text = score1 + score2 + score3 + "";
+                    TextView scorePlayer1 = (TextView) findViewById(R.id.scoreSpeler1);
+                    scorePlayer1.setText(score1Text);
+
                     if (tp1_number == 3) {
                         playerOne = false;
                         playerOneIndicator.setVisibility(View.INVISIBLE);
@@ -302,6 +345,11 @@ public class Main2Activity extends AppCompatActivity {
                     String tp2_new_text = tp2_number + "";
                     turnPlayer2.setText(tp2_new_text);
                     tp1_number = tp1_number-1;
+
+                    String score2Text = score1 + score2 + score3 + "";
+                    TextView scorePlayer2 = (TextView) findViewById(R.id.scoreSpeler2);
+                    scorePlayer2.setText(score2Text);
+
                     if (tp2_number-1 == tp1_number) {
                         // resetfunctie in de plaats zetten
                         playerOneIndicator.setVisibility(View.VISIBLE);
@@ -319,6 +367,57 @@ public class Main2Activity extends AppCompatActivity {
 
     public static int randomDiceValue() {
         return RANDOM.nextInt(6) + 1;
+    }
+
+    public int howMuch1() {
+        if (value1 == 1) {
+            score1 = 100;
+        } else if (value1 == 2) {
+            score1 = 2;
+        } else if (value1 == 3) {
+            score1 = 3;
+        } else if (value1 == 4) {
+            score1 = 4;
+        } else if (value1 == 5) {
+            score1 = 5;
+        } else if (value1 == 6) {
+            score1 = 60;
+        }
+        return score1;
+    }
+
+    public int howMuch2() {
+        if (value2 == 1) {
+            score2 = 100;
+        } else if (value2 == 2) {
+            score2 = 2;
+        } else if (value2 == 3) {
+            score2 = 3;
+        } else if (value2 == 4) {
+            score2 = 4;
+        } else if (value2 == 5) {
+            score2 = 5;
+        } else if (value2 == 6) {
+            score2 = 60;
+        }
+        return score2;
+    }
+
+    public int howMuch3() {
+        if (value3 == 1) {
+            score3 = 100;
+        } else if (value3 == 2) {
+            score3 = 2;
+        } else if (value3 == 3) {
+            score3 = 3;
+        } else if (value3 == 4) {
+            score3 = 4;
+        } else if (value3 == 5) {
+            score3 = 5;
+        } else if (value3 == 6) {
+            score3 = 60;
+        }
+        return score3;
     }
 
 
